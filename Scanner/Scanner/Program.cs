05be6c409361a0b399d087b2e5541bcfe3795caa
Your Name <you@example.com>
2025-02-04 13:01:09 +0300

    Изменено

--- Scanner/Scanner/Program.cs
+++ Scanner/Scanner/Program.cs
@@ -6,7 +6,7 @@ namespace Scanner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi!");
         }
     }
 }
